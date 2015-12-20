5db5ac72dffd05b2d7c8439654bb609aedcc4547
Small Bob <bob@email.com>
2015-12-20 23:51:19 +0300

    Initial

---
+++ main.cpp
@@ -0,0 +1,37 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  generator for model tests
+ *
+ *        Version:  1.0
+ *        Created:  18.12.2015 23:49:12
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include	<iostream>
+#include  "modelgen.h"
+
+using namespace std;
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  
+ * =====================================================================================
+ */
+	int
+main ( int argc, char *argv[] )
+{
+	ModelGenerator gen;
+	cout << "---------------------\n";
+	cout << "New test script file.\n";
+	cout << "---------------------\n";
+	return 0;
+}				/* ----------  end of function main  ---------- */
---
+++ modelgen.cpp
@@ -0,0 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  modelgen.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  19.12.2015 13:36:25
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <iostream>
+#include "modelgen.h"
+using namespace std;
+
+/*
+ *--------------------------------------------------------------------------------------
+ *       Class:  ModelGenerator
+ *      Method:  ModelGenerator
+ * Description:  constructor
+ *--------------------------------------------------------------------------------------
+ */
+ModelGenerator::ModelGenerator ()
+{
+	cout << "New Test generation started.\n";
+
+}  /* -----  end of method ModelGenerator::ModelGenerator  (constructor)  ----- */
+
+
